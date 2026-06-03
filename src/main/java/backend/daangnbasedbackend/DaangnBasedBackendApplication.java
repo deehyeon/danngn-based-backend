55c9b95f8a3595410ac2770b3b254704266e8b32
Jihyeon An <jh011007@naver.com>
2026-06-03 13:25:32 +0900

    feat: init project

---
+++ src/main/java/backend/daangnbasedbackend/DaangnBasedBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.daangnbasedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnBasedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaangnBasedBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/daangnbasedbackend/DaangnBasedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.daangnbasedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnBasedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
