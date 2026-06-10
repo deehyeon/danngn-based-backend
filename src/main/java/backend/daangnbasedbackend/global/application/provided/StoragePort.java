8f24f26bf4e494bb598d52f128487abd1ca29c94
Jihyeon An <jh011007@naver.com>
2026-06-10 21:05:06 +0900

    REFACTOR: StorageController를 StorageApi 인터페이스로 분리

--- src/main/java/backend/daangnbasedbackend/global/application/provided/StoragePort.java
+++ src/main/java/backend/daangnbasedbackend/global/application/provided/StoragePort.java
@@ -5,5 +5,5 @@ import backend.daangnbasedbackend.global.application.dto.PresignedUrlRes;
 public interface StoragePort {
     String upload(String directory, String originalFilename, byte[] bytes, String contentType);
     void delete(String fileUrl);
-    PresignedUrlRes generatePresignedUrl(String directory, String originalFilename, String contentType);
+    PresignedUrlRes generatePresignedUrl(String directory, Long memberId, String originalFilename, String contentType);
 }
